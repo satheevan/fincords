76b7ec1f64a54431794fbcef5985e9ee474b6c4f
saranquan <saravanan.r@quantique.ai>
2024-01-08 10:17:34 +0530

    updating the custom button

---
+++ src/components/shared/customButton/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './button.css'
+
+const CustomButton=({title,classNameBtn,onClickHandler,...props}) =>{
+  return (
+    <div className={props.containerClassName || ""}>
+    <button type='button' className={`button ${classNameBtn || ""}`} onClick={onClickHandler}>{title || ""}</button>
+    </div>
+  )
+}
+
+export default CustomButton
